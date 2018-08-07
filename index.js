89429c397648e8250ad3674cda466cbe2cc336e0
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-08-07 21:24:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-const ul1 = document.getElementsByTagName('ul')[0]
-const li1 = ul1.children[0]
-const ul2 = document.getElementsByTagName('ul')[1]
-const li2 = ul2.children[0]
+function getFirstSelector(selector){
+  
+  return document.querySelector(selectr)
+}
+function nestedTarget(){
+  
+  return document.querySelector('#nested .target')
+}
+ function increaseRankBy(n){
+let ranking= document.querySelectorAll('.ranked-list li')
+ for(let i=0;i<ranking.length;i++){
+   ranking[i].innerHTML = parseInt(ranking[i].innerHTML)+n
+   
+ }
+ }
+  function deepestChild(){
+    let grand = document.querySelector('div#grand-node')
+    let deepest = deepest.children[0]
+    for(let i=0 ; !deepest.children[i];i){
+      deepest = deepest.children[0]
+      return deepest;
+    }
+  }
